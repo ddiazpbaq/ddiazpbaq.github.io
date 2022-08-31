1df6b09b249f87412600e4f567b62ac8ee1a0d57
ddiazpbaq <109553358+ddiazpbaq@users.noreply.github.com>
2022-08-31 10:12:23 -0500

    cambio de img de glp por map

--- gl.php
+++ gl.php
@@ -318,7 +318,7 @@ body{  background-color: #00a6ce;  }
       <div class="col">
 
       <div class="card card-primary text-center">
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1k-oc7pnnbFkiw9rnC2ffbfgqPQS5qQHL" alt="Card image cap">
+        <img class="card-img-top" src="https://drive.google.com/uc?id=10nYlUMu4pB-MpLSntJHVsjDBhEdYEpZn" alt="Card image cap"> 
         <!-- https://static3.avast.com/10001215/web/i/index2/for-partners.jpg -->
         <!--https://static3.avast.com/10001215/web/i/index2/for-home.jpg--> 
         <div class="card-body">
